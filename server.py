2a43aae1678ca74f25b97667e33c55a0e258e7c6
JohnThirdt <lvantretiyakov2018@gmail.com>
2024-02-27 16:40:28 +0300

    Добавить  gitignore

--- server.py
+++ server.py
@@ -1,31 +1,15 @@
 import socket
 import logging
-import json
-import random
-import datetime
 
-from common import ADDR
 
 logger = logging.getLogger(__name__)
-logging.basicConfig()
+logging.basicConfig(level = logging.INFO)
 
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
-c = s.bind(ADDR) 
-s.listen(5)
-
-s.setblocking(False) 
-s.settimeout(1)
-
+s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+s.bind(('0.0.0.0', 10000))
 while True:
-    try:
-        client_socket = s.accept()
-    except TimeoutError:
-        continue
-    client, (addr, port) = client_socket
-    logger.warning(f"{addr} connected from port {port}")
+    message, (ip, port) = s.recvfrom(500)
+    logger.info(f"{ip}: {message.decode()}")
 
-    data = {"time": datetime.datetime.now().isoformat(), "number": random.random()}
 
-    client.send(json.dumps(data).encode())
-    client.close()
+
